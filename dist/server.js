12459b3554dfee0ac64cbcba8e28996721a5bd97
iki789 <ikhlak789@gmail.com>
2025-03-10 12:31:42 -0700

    resolved exervise

--- dist/server.js
+++ dist/server.js
@@ -1,2 +1,31 @@
 "use strict";
-// This is your server file :)
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const path_1 = __importDefault(require("path"));
+dotenv_1.default.config();
+const app = (0, express_1.default)();
+app.set("view engine", "ejs");
+app.set("views", path_1.default.join(__dirname, "../src/views"));
+app.use(express_1.default.static(path_1.default.join(__dirname, "public")));
+app.get("/", (req, res) => {
+    res.render("home", {
+        title: "Home",
+    });
+});
+app.get("/about", (req, res) => {
+    res.render("about", {
+        title: "About",
+    });
+});
+app.get("/contact", (req, res) => {
+    res.render("contact", {
+        title: "Contact",
+    });
+});
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on http://localhost:${process.env.PORT}`);
+});
--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,32 @@
-// This is your server file :)
+import express from "express";
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config();
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "../src/views"));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.render("home", {
+    title: "Home",
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About",
+  });
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact", {
+    title: "Contact",
+  });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on http://localhost:${process.env.PORT}`);
+});
